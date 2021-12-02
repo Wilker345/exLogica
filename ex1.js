eb91f41f621602b3472527fc0e58b83d29b515ac
Carlos Wilker <74657679+Wilker345@users.noreply.github.com>
2021-12-01 22:51:50 -0300

    Create ex1.js
    registro de operadores lógicos e relacionais em JS, para evitar confusão com outra linguagens

---
+++ ex1.js
@@ -0,0 +1,17 @@
+/*
+
+Operador	Descrição	Exemplo
+==	Igual	(A == B) é false
+===	Igual (tanto o valor quanto o tipo de objeto)	(A === B) é false
+!=	Não igual	(A != B) é true
+>	Maior que	(A > B) é false
+<	Menor que	(A < B) é true
+>=	Maior ou igual que	(A >= B) é false
+<=	Menor ou igual que	(A <= B) é true
+
+
+Operador	Descrição	Exemplo
+&&	Operador AND (E)	(A && B) é false
+||	Operador OR (OU)	(A || B) é true
+!	Operador NOT (NÃO) ou Negação	!(A && B) é true
+*/
